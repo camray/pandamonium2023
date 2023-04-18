662c2ed6b3d58ef262c0068b6b5d29c8bcceb366
Cameron Ray <cameron.ray@instructure.com>
2023-04-18 12:23:31 -0400

    add more projects

---
+++ v3/src/lib.rs
@@ -0,0 +1,24 @@
+use wasm_bindgen::prelude::*;
+use web_sys::{MouseEvent};
+
+#[wasm_bindgen]
+pub fn start() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("No global `window` exists.");
+    let document = window
+        .document()
+        .expect("Should have a document on the window.");
+
+    let closure = Closure::wrap(Box::new(move |_event: MouseEvent| {
+        web_sys::console::log_1(&"Button clicked!".into())
+    }) as Box<dyn FnMut(MouseEvent)>);
+
+    document
+        .query_selector_all("button")?
+        .get(0)
+        .unwrap()
+        .add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+
+    closure.forget();
+
+    Ok(())
+}
